70cf2056119fefb23904131f8b5d2eb02494ef3a
OLDYTWEBSITE <145997493+OLDYTWEBSITE@users.noreply.github.com>
2023-10-20 10:18:38 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -39,7 +39,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Sorry, your file was not uploaded.";
   } else {
     if (move_uploaded_file($_FILES["video"]["tmp_name"], $targetFile)) {
-      echo "The file " . basename($_FILES["video"]["name"]) . " has been uploaded.";
+      $videoId = pathinfo($targetFile, PATHINFO_FILENAME); // Extract the video ID from the uploaded file's name
+
+      // Generate the HTML file name based on the video ID
+      $htmlFileName = 'Video_Id=' . $videoId . '.html';
+
+      // Generate the video URL based on the video ID
+      $videoUrl = 'https://oldytwebsite.github.io/Video_Id=' . $videoId;
+
+      // Generate the HTML content by replacing the VIDEO_URL placeholder with the actual video URL
+      $htmlContent = str_replace('VIDEO_URL', $videoUrl, file_get_contents('video.html'));
+
+      // Save the generated HTML content to a file
+      file_put_contents($htmlFileName, $htmlContent);
+
+      // Redirect to the generated HTML page
+      header('Location: ' . $htmlFileName);
+      exit;
     } else {
       echo "Sorry, there was an error uploading your file.";
     }
